1a3f125d7b3a55f9f88f318a4e2735ec187aab99
UchaBokeria <ucha1bokeria@gmail.com>
2022-03-13 07:05:38 -0700

    x

--- Services/Guarder/Guarder.php
+++ Services/Guarder/Guarder.php
@@ -6,11 +6,12 @@ class Guard extends Database
     public function checkToken()
     {
 
-        if($_SERVER["HTTP_TOKEN"]== "") $Result = false;
-        else {
-            $UserInfo = parent::GET(" SELECT id FROM users WHERE token = :token ;", [ 'token' => $_SERVER["HTTP_TOKEN"] ]);
-            $Result = parent::Exists();
-        }
+        if($_SERVER["HTTP_TOKEN"]== "") 
+            return [ 'error' => false , 'msg' => 'Empty Token' ];
+
+        $UserInfo = parent::GET(" SELECT id FROM users WHERE token = :token ;", [ 'token' => $_SERVER["HTTP_TOKEN"] ]);
+        $Result = parent::Exists();
+
         
         return [ 'error' => !$Result , 'msg' => ($Result) ? 'Access granted' : 'Wrong Token' ];
 
