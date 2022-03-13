e285805cf0eb73de6a4494a45b6e214817c62df7
UchaBokeria <ucha1bokeria@gmail.com>
2022-03-13 05:12:37 -0700

    x

--- Services/AutoLoader/AutoLoader.php
+++ Services/AutoLoader/AutoLoader.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register( function($Controller){
-    $Path = str_replace('\\', '/', CONTROLLERS_SCHEME[$Controller]);
+    $Path = str_replace(DIRECTORY_SEPARATOR , '/', CONTROLLERS_SCHEME[$Controller]);
     if(!file_exists(".$Path")) echo die(" >> `$Controller` In `$Path` Does Not Exist");
     else include_once ".$Path";
 });
