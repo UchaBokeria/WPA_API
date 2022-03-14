d7f5c31493ca8b4af4705a2fe07c72ac182f8e57
UchaBokeria <ucha1bokeria@gmail.com>
2022-03-14 11:57:58 -0700

    x

--- Serve.php
+++ Serve.php
@@ -1,10 +1,42 @@
 <?php
-header("Location:testmail.php");
-/* Configs */
-require "./Services/Engineer/LearnConfigs.Scheme.php";
-require "./Services/Engineer/LearnControllers.Scheme.php";
 
-/* Services */
+// /* Configs */
+// require "./Services/Engineer/LearnConfigs.Scheme.php";
+// require "./Services/Engineer/LearnControllers.Scheme.php";
+
+// /* Services */
+// require "./Services/Database/Database.php";
+// require "./Services/Mailer/SmtpMailer.php";
+// require "./Services/Engineer/API.Core.php";
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+define('MAILSMTPDEBUG', 2);
+define('MAILSMTPAUTH', true);
+define('MAILUSERNAME', 'support@wpatbilisicongress.com');
+define('MAILFORMNAME', 'support@wpatbilisicongress.com');
+define('MAILPASSWORD', 'Wpatbilisi2022!');
+define('MAILSMTPSECURE', 'tls');
+define('MAILDEBUGOUTPUT', 'html');
+define('MAILHOST', 'ssl://smtp.titan.support@wpatbilisicongress.com');
+define('MAILCHARSET', 'UTF-8');
+define('MAILPORT', 465);
+
+require "./Config/Mailer.Confing";
+require "./Config/Mailer.Confing";
+
 require "./Services/Database/Database.php";
 require "./Services/Mailer/SmtpMailer.php";
-require "./Services/Engineer/API.Core.php";
+
+
+var_dump( (new SmtpMailer([
+    'cc_address' => 'TEST cc_address',
+    'bcc_address' => 'TEST bcc_address',
+    'address' => 'ucha1bokeria@gmail.com',
+    'subject' => 'TEST subject',
+    'body' => 'TEST body'
+]))->Send()
+);
