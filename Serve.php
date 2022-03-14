e415d757917529f2da3417022663c53d8e1e454d
UchaBokeria <ucha1bokeria@gmail.com>
2022-03-14 12:15:38 -0700

    x

--- Serve.php
+++ Serve.php
@@ -19,11 +19,11 @@ define('MAILSMTPAUTH', true);
 define('MAILUSERNAME', 'wpatbilisicongress@gmail.com');
 define('MAILFORMNAME', 'wpatbilisicongress@gmail.com');
 define('MAILPASSWORD', 'wpatbilisi2022');
-define('MAILSMTPSECURE', 'ssl');
+define('MAILSMTPSECURE', 'tls');
 define('MAILDEBUGOUTPUT', 'html');
 define('MAILHOST', 'smtp.gmail.com');
 define('MAILCHARSET', 'UTF-8');
-define('MAILPORT', 465);
+define('MAILPORT', 587);
 
 require "./Config/Mailer.Config.php";
 require "./Config/MySQL.Config.php";
