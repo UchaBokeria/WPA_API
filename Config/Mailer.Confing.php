3bbedcdc9c95d4f646d2f9fd0272fade1a25695c
UchaBokeria <ucha1bokeria@gmail.com>
2022-03-14 11:34:50 -0700

    x

--- Config/Mailer.Confing.php
+++ Config/Mailer.Confing.php
@@ -2,11 +2,11 @@
 
 define('MAILSMTPDEBUG', 2);
 define('MAILSMTPAUTH', true);
-define('MAILUSERNAME', 'support@elitelectronics.ge');
-define('MAILFORMNAME', 'support@elitelectronics.ge');
+define('MAILUSERNAME', 'wpatbilisicongress@gmail.com');
+define('MAILFORMNAME', 'support@wpa.ge');
 define('MAILPASSWORD', 'Wpatbilisi2022!');
 define('MAILSMTPSECURE', 'tls');
 define('MAILDEBUGOUTPUT', 'html');
-define('MAILHOST', 'smtp.titan.email');
+define('MAILHOST', 'smtp.hostinger.email');
 define('MAILCHARSET', 'UTF-8');
 define('MAILPORT', 465);
