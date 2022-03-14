f5a1a519b565df0fff719862183546de49b78297
UchaBokeria <ucha1bokeria@gmail.com>
2022-03-14 10:09:46 -0700

    x

--- Services/Engineer/LearnConfigs.Scheme.php
+++ Services/Engineer/LearnConfigs.Scheme.php
@@ -32,9 +32,7 @@ function LearnConfigs($dir){
                 realpath(".".$dir.DIRECTORY_SEPARATOR.$ff)) 
             );
 
-            include_once ".".str_replace(DIRECTORY_SEPARATOR , '/', str_replace( ABSOLUTEPATH, '' , str_replace(
-                '/home/u609332810/domains/wpatbilisicongress.com/public_html/Server','',
-                realpath(".".$dir.DIRECTORY_SEPARATOR.$ff))));
+            var_dump($Configs[$name]);die();
         }
     }
 
