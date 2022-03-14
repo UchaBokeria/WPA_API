56f4be82f2bfae46f5310e5041633220adf59dd4
UchaBokeria <ucha1bokeria@gmail.com>
2022-03-14 11:09:35 -0700

    x

--- Serve.php
+++ Serve.php
@@ -5,6 +5,6 @@ require "./Services/Engineer/LearnConfigs.Scheme.php";
 require "./Services/Engineer/LearnControllers.Scheme.php";
 
 /* Services */
-require "./Services/Mailer/SmtpMailer.php";
 require "./Services/Database/Database.php";
+require "./Services/Mailer/SmtpMailer.php";
 require "./Services/Engineer/API.Core.php";
