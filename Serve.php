4d9cae35f0189f9c3a264c4c0683aee80c04f0b0
UchaBokeria <ucha1bokeria@gmail.com>
2022-03-14 12:02:46 -0700

    x

--- Serve.php
+++ Serve.php
@@ -19,7 +19,7 @@ define('MAILSMTPAUTH', true);
 define('MAILUSERNAME', 'support@wpatbilisicongress.com');
 define('MAILFORMNAME', 'support@wpatbilisicongress.com');
 define('MAILPASSWORD', 'Wpatbilisi2022!');
-define('MAILSMTPSECURE', 'tls');
+define('MAILSMTPSECURE', 'ssl');
 define('MAILDEBUGOUTPUT', 'html');
 define('MAILHOST', 'ssl://smtp.titan.support@wpatbilisicongress.com');
 define('MAILCHARSET', 'UTF-8');
