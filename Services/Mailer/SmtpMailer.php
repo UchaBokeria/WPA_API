d9290f7edcbc25218dcab65e700de359be181dc9
uchaia ucha <uch1bokeria@gmail.com>
2022-03-15 18:48:56 +0400

    x

--- Services/Mailer/SmtpMailer.php
+++ Services/Mailer/SmtpMailer.php
@@ -84,8 +84,8 @@
         public function TemplateBuild($object, $template)
         {
             foreach ($object as $key => $value)
-                $template = str_replace("{$key}", $value, $template);
-            
+                $template = str_replace("{". $key . "}", $value, $template);
+            var_dump($template);die();
             return $template;
         }
 
