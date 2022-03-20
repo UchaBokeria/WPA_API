697d77597ef268193f9a4231466766e7e025d646
UchaBokeria <ucha1bokeria@gmail.com>
2022-03-20 05:07:09 -0700

    x

--- Services/Mailer/SmtpMailer.php
+++ Services/Mailer/SmtpMailer.php
@@ -77,7 +77,7 @@
             // );
 
             $mail->isHTML(true);
-            $mail->Body = '<img src="https://doc-14-a0-docs.googleusercontent.com/docs/securesc/jagmva5ufaire31mv4rfvapg01jf1r2c/9rb8hhg75mo8hvuvedhdp1ivpr7tli8n/1647777675000/17593894767674285516/17593894767674285516/16fVr0AeqN4Mrw575YSthG4ITmv8U3oQS?e=download&ax=ACxEAsa9ys-DT7jTRUNyxtEnvAcRnQB3FWNaxH2VWqP0lYNqWMdjVMLYE7ElilLZrZEKMJtgdQKzPzzoZB7UjA7AJqbvycAaKm5RC2qr3-kYgh5TE24ElAlb2nOFj0S99DBkNvPzZzBa6mZGyWtsMFhfIsxAwN0F_01OlpwvuUHlx1DcPiOIWwxVakQxK0qE9gkFGBsxm1JxArqQEVCW5CaSCHqerlZ84ERX_86gA90AtC5amUkhm6koHR9Zz_ggfnYhTnQz_JVTxW5I-9mXMg3UQeB11eqgILUY4GZ4kGlt2arZMvN1aVgqqGEFjKWSiCJ1FXurJyv1o1Ce6NNM-_nkOElgfReE1j8mJPMB6cQTUO-4nI1hs8Yx8l-Y5w84VW12iNv7iL7BO5c3zS-RUAK-IHuG9dN8WKkh-5A5SoYoJW5_N782dVlvgg445tc1zpIU1Ymq65iFqwUaLuhfCFYbtx7KjkW7x_gQn0DX_TztvxGTGCOOVjvZb8ZI0S3bsemNgO_3DvGwKe_F5OkOK_CcexNZ4_x8zJp432wSBrmggQQX_m02-LWZy0TfRn7trSjBCnKc2wR9GvvOZpFLynOIWFdNNTM7hrBrG6kVocyXrSXf7wu1vJJobl__cjcLviFVDHW7uc6eLRizcW-dc0_TK2Juor5krMziUVd2QcSN94roUYF4TqzK-cfSSnPx0-ryc_flR6qfIpZjOy6YTN5KfZ-RnyfGKguT17y2OEVUW69UQJ-JloOd-xL5BvE2MDoj3q-r-6WnIMFX5lRx3x4hQu8&authuser=2&nonce=c6rlalk4jdnha&user=17593894767674285516&hash=u7gfqmp87pkk2qd2n7rttenrtargmhv9"> X';
+            $mail->Body = '<img src="https://th.bing.com/th/id/OIP.XVFooFAPijjQp2-9vm5ttQHaHa?pid=ImgDet&w=614&h=614&rs=1"> X';
             //$mail->addAttachment("./Sources/Doc/logosvg.svg","logosvg"); 
 
             $resp = $mail->send();
