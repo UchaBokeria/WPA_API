3d5858cce431c9380317954106e3dd823ae1e9ac
UchaBokeria <ucha1bokeria@gmail.com>
2022-03-30 14:43:32 -0700

    x

--- Services/Engineer/Decorators.Module.php
+++ Services/Engineer/Decorators.Module.php
@@ -1,8 +1,11 @@
 <?php
+$raw = json_decode(file_get_contents('php://input'), TRUE);
+if($raw != null) {
+    if($_SERVER["REQUEST_METHOD"] == 'POST')
+        $_POST = array_merge($_POST, $raw);
+    
+    
+    if($_SERVER["REQUEST_METHOD"] == 'GET')
+        $_GET = array_merge($_GET, $raw);
 
-if($_SERVER["REQUEST_METHOD"] == 'POST')
-    $_POST = array_merge($_POST, json_decode(file_get_contents('php://input'), TRUE));
-
-
-if($_SERVER["REQUEST_METHOD"] == 'GET')
-    $_GET = array_merge($_GET, json_decode(file_get_contents('php://input'), TRUE));
+}
