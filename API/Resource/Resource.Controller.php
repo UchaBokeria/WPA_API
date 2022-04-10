5e7ff59308361be7d66204d3ebe8a88ef78750d5
UchaBokeria <ucha1bokeria@gmail.com>
2022-04-10 04:44:00 -0700

    x

--- API/Resource/Resource.Controller.php
+++ API/Resource/Resource.Controller.php
@@ -6,7 +6,7 @@
         public function GetFile()
         {
             $path = $_POST["path"];
-            header('Location: $path');
+            header("Location: $path");
         }
 
        
