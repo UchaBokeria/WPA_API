203609d339089c2beeeec2d5ad3d98bcb905ff36
UchaBokeria <ucha1bokeria@gmail.com>
2022-06-19 08:00:36 -0700

    ax

--- API/Banking/Banking.Controller.php
+++ API/Banking/Banking.Controller.php
@@ -39,9 +39,7 @@ class Banking extends Database
 
     private function GetToken() 
     {
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ERROR);
+
         $this->TOKEN = $this->TBC->request([
           'Body' => '',
           'Method' => 'POST',
