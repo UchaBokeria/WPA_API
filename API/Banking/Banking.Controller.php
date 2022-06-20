02f047a37e8044cff67d8efa527f2fac774b08bf
uchabokeria <ucha1bokeria@gmail.com>
2022-06-20 15:26:49 +0400

    fx

--- API/Banking/Banking.Controller.php
+++ API/Banking/Banking.Controller.php
@@ -45,24 +45,23 @@ class Banking extends Database
     {
 
         $curl = curl_init();
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => "$this->url/access-token",
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => '',
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 0,
-          CURLOPT_FOLLOWLOCATION => true,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => 'POST',
-          CURLOPT_POSTFIELDS => "client_Id=$this->clientID&client_secret=$this->SECRET",
-          CURLOPT_HTTPHEADER => array(
+        curl_setopt_array($curl, [
+            CURLOPT_URL => "$this->url/access-token",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => "client_Id=$this->clientID&client_secret=$this->SECRET",
+            CURLOPT_HTTPHEADER => array(
             'Content-Type: application/x-www-form-urlencoded',
             'apikey: '.$this->APIKEY,
-          ),
-        ));
+            ),
+        ]);
         
-        $response = curl_exec($curl);
-        $this->TOKEN = json_decode($response,true)["access_token"];
+        $this->TOKEN = json_decode(curl_exec($curl),true)["access_token"];
         return $this->TOKEN;
 
     }
