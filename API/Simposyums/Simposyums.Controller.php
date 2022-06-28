73c07b4748d8d7658fc0bc1abce56a9e5fb56ee7
uchabokeria <ucha1bokeria@gmail.com>
2022-06-28 12:43:17 +0400

    fx

--- API/Simposyums/Simposyums.Controller.php
+++ API/Simposyums/Simposyums.Controller.php
@@ -46,7 +46,7 @@
                 $index = 1;
                 foreach ($value['presentator'] as $prese) {
                     foreach ($prese as $key => $Templateval) 
-                        $value["presentator_$key"."_"."$index"] = json_decode($Templateval,true);
+                        $value["presentator_$key"."_"."$index"] = json_decode(stripslashes($Templateval),true);
                         $index++;
                     }
 
