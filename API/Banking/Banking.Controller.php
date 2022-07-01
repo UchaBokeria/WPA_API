e471f5ce910b68df25df3bbc09ead99abfe41a65
uchabokeria <ucha1bokeria@gmail.com>
2022-07-01 12:08:31 +0400

    fx

--- API/Banking/Banking.Controller.php
+++ API/Banking/Banking.Controller.php
@@ -111,7 +111,7 @@ class Banking extends Database
           CURLOPT_POSTFIELDS =>'
             {
                 "amount": {
-                    "currency": "GEL",
+                    "currency": "EUR",
                     "total": ' . $Product["price"] . '
                 },
                 "installmentProducts": [' . json_encode($Product) . '],
